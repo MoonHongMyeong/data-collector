0cbf2e194e8a1780199349e7be92435f396fae70
MoonHongMyeong <moonhongmyeong@gmail.com>
2022-02-24 16:16:20 +0900

    새로운 타이머 프로세스 적용 메인문

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import time
-from multiprocessing import Process, Queue
+from multiprocessing import Process, Queue, Pipe
 
 from collector.cpu_collector import CpuCollector
 from collector.memory_collector import MemoryCollector
 from collector.storage_collector import StorageCollector
-from timer.repeated_timer import RepeatedTimer
+from timer.custom_timer import CustomTimer
 from utils.socketclient import Client
 from utils.config_reader import ConfigReader
 
@@ -16,45 +16,47 @@ if __name__ == '__main__':
     IP, PORT = cr.get_ip_port()
 
     que = Queue()
-    client = Client(que)
 
+    client = Client(que)
     client.connect(IP, PORT)
 
+    pipes = []
+    index = 1
+
     for process in processes:
+        locals()['p_con' + str(index)], locals()['c_con' + str(index)] = Pipe()
+        pipes.append((process['type'], process['interval'], locals()['p_con' + str(index)]))
+
         if process['type'] == 1:
-            locals()[process['name']] = CpuCollector(que)
+            locals()[process['name']] = CpuCollector(que, locals()['c_con' + str(index)])
         elif process['type'] == 2:
-            locals()[process['name']] = MemoryCollector(que)
+            locals()[process['name']] = MemoryCollector(que, locals()['c_con' + str(index)])
         elif process['type'] == 3:
-            locals()[process['name']] = StorageCollector(que)
+            locals()[process['name']] = StorageCollector(que, locals()['c_con' + str(index)])
 
-    set_process_idx = 1
-    for process in processes:
-        locals()['rt'+str(set_process_idx)] = \
-            RepeatedTimer(process['interval'], locals()[process['name']].start)
-        locals()['p'+str(set_process_idx)] = \
-            Process(target=locals()['rt'+str(set_process_idx)].start)
-        set_process_idx += 1
+        locals()['p' + str(index)] = Process(target=locals()[process['name']].start)
+        locals()['p' + str(index)].start()
 
-    set_start_process_idx = 1
-    for process in processes:
-        locals()['p'+str(set_start_process_idx)].start()
-        set_start_process_idx += 1
+        index += 1
 
     socket_process = Process(target=client.send)
     socket_process.start()
+
+    timer = CustomTimer(pipes)
+    timer_process = Process(target=timer.start)
+    timer_process.start()
 
     while 1:
 
         time.sleep(1)
 
-        set_start_process_idx = 1
+        start_process_idx = 1
         for process in processes:
 
-            if not locals()['p' + str(set_start_process_idx)].is_alive():
-                locals()['p' + str(set_start_process_idx)].start()
+            if not locals()['p' + str(start_process_idx)].is_alive():
+                locals()['p' + str(start_process_idx)].start()
 
-            set_start_process_idx += 1
+            start_process_idx += 1
 
         if not socket_process.is_alive():
             socket_process.start()
