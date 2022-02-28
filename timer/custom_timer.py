407aad26f72b4b41254983c85fd1e0418ae3d9db
MoonHongMyeong <moonhongmyeong@gmail.com>
2022-02-28 09:46:56 +0900

    Refactor : 중복되는 구문 제거

--- timer/custom_timer.py
+++ timer/custom_timer.py
@@ -14,10 +14,5 @@ class CustomTimer:
             self._next_call += 1
             for process in self._processes:
                 if self._count % (process[1] / 1000) == 0:
-                    if process[0] == 1:
-                        process[2].send(0)
-                    elif process[0] == 2:
-                        process[2].send(0)
-                    elif process[0] == 3:
-                        process[2].send(0)
+                    process[2].send(0)
             time.sleep(self._next_call - time.time())
